4057e1658b25b4956b5b741e6c9b7e38627ad3db
Theresa <thetheresagarcia@gmail.com>
2019-12-26 20:12:11 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,7 +5,7 @@ def nyc_pigeon_organizer(data)
     value.each do |new_value, names|
       names.each do |name|
 
-        if !new_hash[name] 
+        if !new_hash[name]
           new_hash[name] = {}
         end
 
@@ -14,7 +14,7 @@ def nyc_pigeon_organizer(data)
         end
         new_hash[name][key] << new_value.to_s
       end
-    end    
+    end
   end
   new_hash
 end
